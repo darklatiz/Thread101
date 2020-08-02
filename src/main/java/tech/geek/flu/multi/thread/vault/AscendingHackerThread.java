e5e50b1c722ceccd948be36a9d1b2141dd06522d
Spike Storm <software.j2ee@gmail.com>
2020-08-02 14:10:51 -0700

    amend

--- src/main/java/tech/geek/flu/multi/thread/vault/AscendingHackerThread.java
+++ src/main/java/tech/geek/flu/multi/thread/vault/AscendingHackerThread.java
@@ -14,6 +14,7 @@ public class AscendingHackerThread extends Hacker{
                 System.exit(0);
             }else {
                 System.out.println("Password tried, it did not work: " + i + " , by Hacker: " + this.getName());
+
             }
         }
     }
