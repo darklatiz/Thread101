54e372ec7fc106f3a36e3436d7e927f4769b016a
Luis Gonzalez <Luisenrique.Gonzalezhernandez@walmartlabs.com>
2020-08-09 15:36:24 -0700

    References

--- src/main/java/tech/geek/flu/lambda/referece/ConstructorReference.java
+++ src/main/java/tech/geek/flu/lambda/referece/ConstructorReference.java
@@ -1,9 +1,21 @@
 package tech.geek.flu.lambda.referece;
 
+import java.util.function.Function;
+
 public class ConstructorReference {
 
   public static void main(String[] args) {
+    Function<Runnable, Thread>  threadGenerator = Thread :: new;
+    Runnable task1 = () -> System.out.println("Task1 Executed");
+    Runnable task2 = () -> System.out.println("Task2 Executed");
 
+    Thread thread  = threadGenerator.apply(task1);
+    Thread thread1 = threadGenerator.apply(task2);
+
+    thread.start();
+    thread1.start();
+
+    threadGenerator.apply(() -> System.out.println("Task3 Executed")).start();
   }
 
 }
